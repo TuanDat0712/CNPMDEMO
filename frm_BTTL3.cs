2b253bef8a3b3159d15d84b482192a1055621137
TuanDat0712 <145252507+TuanDat0712@users.noreply.github.com>
2023-09-17 14:36:02 +0700

    Add files via upload

---
+++ frm_BTTL3.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _32_713_TranLeTuanDat_Tuan01
+{
+    public partial class frm_BTTL3 : Form
+    {
+        public frm_BTTL3()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void frm_BTTL3_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
